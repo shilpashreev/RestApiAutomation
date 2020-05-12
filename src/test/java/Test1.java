e10483968e146ea2839c1b358029939561967d9d
shilpashree <shilpashree@shilpashrees-MacBook-Air.local>
2020-05-12 16:40:48 +0530

    rest assured initial commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,20 @@
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+
+public class Test1 {
+
+    @Test
+    public void FormulaTest1(){
+         RestAssured.given()
+                 .when()
+                    .get("http://ergast.com/api/f1/2017/circuits.json")
+                 .then()
+                    .assertThat()
+                    .body()
+
+
+    }
+
+
+}
